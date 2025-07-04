77ec04ae639f582671064589a61f0f51ca1ae4c9
manukotari <manojkumarkumar01189@gmail.com>
2025-07-05 00:12:29 +0530

    nav bar updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav'
+
+  /* The following line can be included in your src/index.js or App.js file */
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    <div id='container'>
-      <h3>To-Do List</h3>
+      <Nav className="justify-content-end" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link className='nav_item
+          ' href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='nav_item
+          ' eventKey="link-1">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='nav_item
+          ' eventKey="link-2">More</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='nav_item
+          ' eventKey="disabled" >
+            Logout
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
    </div>
   );
 }
